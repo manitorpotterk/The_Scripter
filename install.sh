f3004b373c04f0f416ea963b3720e17928493c94
manitorpotterk <45949793+manitorpotterk@users.noreply.github.com>
2020-10-13 03:09:26 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,37 @@
-#!/bin/bash                                                                                                        
-                                                                                                                   
-echo('Installing Stabilize Shell Dependencies')                                                                    
-                                                                                                                   
-apt-get install xdotool                                                                                            
-sudo apt-get install xautomation                                                                                   
-                                                                                                                   
-echo('Installing SSH Cracker Dependencies')                                                                        
-                                                                                                                   
-apt-get install john -y                                                                                            
-                                                                                                                   
-echo('Installing Hash Cracker Dependencies')                                                                       
+#!/bin/bash
+
+echo('Installing Stabilize Shell Dependencies')
+
+apt-get install xdotool
+sudo apt-get install xautomation
+
+echo('Installing SSH Cracker Dependencies')
+
+apt-get install john -y
+
+echo('Installing Hash Cracker Dependencies')
 
 pip3 install futures
 pip3 install argparse
 pip3 install requests
+
+
+chmod +x $PWD/the_scripter.sh
+chmod +x $PWD/scripts/active_enumerator
+chmod +x $PWD/scripts/add_cronjob.sh
+chmod +x $PWD/scripts/add_ssh_key.sh
+chmod +x $PWD/scripts/function.sh
+chmod +x $PWD/scripts/Hash_Cracker.py
+chmod +x $PWD/scripts/myrecon
+chmod +x $PWD/scripts/Password_Genrator.sh
+chmod +x $PWD/scripts/payloadgen.sh
+chmod +x $PWD/scripts/shell_stabalize.sh
+chmod +x $PWD/scripts/ssh_cracker
+chmod +x $PWD/scripts/stabilize_zsh_shell.sh
+chmod +x $PWD/scripts/webserver.sh
+
+
+
+
+
 
