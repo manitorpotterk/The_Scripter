93c3ee719fb5c11a9134c5a2646883fa76fe6983
rahulsinghbisht <59963309+rahulsinghbisht@users.noreply.github.com>
2020-10-15 00:40:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-echo('Installing Stabilize Shell Dependencies')
+echo "Installing Stabilize Shell Dependencies"
 
 apt-get install xdotool
 sudo apt-get install xautomation
 
-echo('Installing SSH Cracker Dependencies')
+echo "Installing SSH Cracker Dependencies"
 
 apt-get install john -y
 
-echo('Installing Hash Cracker Dependencies')
+echo "Installing Hash Cracker Dependencies"
 
 pip3 install futures
 pip3 install argparse
