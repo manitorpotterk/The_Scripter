74740855cc8b590b90a1213f29ea8e862d2c2a57
manitorpotterk <45949793+manitorpotterk@users.noreply.github.com>
2020-10-23 21:44:10 +0530

    Update shell_stabalize.sh

--- scripts/shell_stabalize.sh
+++ scripts/shell_stabalize.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-source "$(pwd)/function.sh"
+source "$(pwd)/scripts/function.sh"
+
+echo 'If you have shell then only it will work . Kill the process if not. '
+
+echo ''
+
 
 hide_guake
+
+echo 'adding tty guys'
 call_cmd "python -c 'import pty; pty.spawn(\"/bin/bash\")'"
 ctrl Z
 call_cmd "stty raw -echo"
